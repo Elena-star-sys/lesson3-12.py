e42bf653eabe48fcc67a1e5fed4d9a7e10313d3a
Elena-star-sys <elenashulgaelena@gmail.com>
2024-07-25 18:28:33 +0300

    Add files via upload

---
+++ Lesson3.py
@@ -0,0 +1,6 @@
+kurs = "Python"
+homework = 12
+clock = 1.5
+time = clock / homework
+print("Курс:" , kurs, ";" , "Количество задач :", homework , ";" ,
+      "Затрачено часов:",clock,";","","Время затраченное на выполнение: ", time)
